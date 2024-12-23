1e3726270296338c722905161cd218fbad366ac5
Bheta <65680585+TheBheta@users.noreply.github.com>
2024-12-23 00:20:50 -0500

    works !!

---
+++ quickcal/init.js
@@ -0,0 +1,13 @@
+chrome.storage.sync.get("apiKey", async ({ apiKey }) => {
+    if (!apiKey) {
+      console.log("No API key found. Please authenticate.");
+      return; // Remain in unauthenticated state
+    } else {
+      document.getElementById("container").innerHTML = 
+      `
+      <h1>QuickCal<\h1>
+      <h4>Your API Key is valid! Enjoy using QuickCal<\h4>
+      <h4>API Key: ${apiKey}
+      `;
+    }
+});
